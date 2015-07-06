quozl/olpc-os-builder

commit b2c425506aa3f9aa5d8a4fe5d15956096df70f9f

sugar: use the microformat updater
Fixes #12878 when used with
gobject-introspection-1.34.2-1.fc18.olpc2.armv7hl.rpm

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -132,3 +132,9 @@ if systemwide:
 else:
     print "echo '%s' > $INSTALL_ROOT/home/olpc/Activities/.groups" % baseurl
 
+print "cat >/usr/share/glib-2.0/schemas/sugar_activity_group.oob.gschema.override <<EOF"
+print "[org.sugarlabs.update]"
+print "backend='microformat.MicroformatUpdater'"
+print "microformat-update-url='%s'" % baseurl
+print "EOF"
+print "/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas"

commit 37703cd0b0280eb3adff81525f76a01e57c89d44

sugar: fix the microformat updater
Microformat updater was not working; did not find activities to update,
because the settings were not in the built system.

Set the GSettings for the updater in the chroot not in the build host,
and use the full versioned group URL not the base URL.

Regression introduced by b2c425506aa3f9aa5d8a4fe5d15956096df70f9f

modules/sugar_activity_group/kspost.60.activities.py
@@ -1,0 +1,20 @@
+# Copyright (C) 2015 One Laptop per Child
+# Licensed under the terms of the GNU GPL v2 or later; see COPYING for details.
+import ooblib
+import urllib
+import urlparse
+
+baseurl = ooblib.read_config('sugar_activity_group', 'url')
+vmaj = int(ooblib.read_config('global', 'olpc_version_major'))
+vmin = int(ooblib.read_config('global', 'olpc_version_minor'))
+vrel = int(ooblib.read_config('global', 'olpc_version_release'))
+suffix = "%d.%d.%d" % (vmaj, vmin, vrel)
+grpurl = urlparse.urljoin(baseurl + "/", urllib.quote(suffix))
+
+print "echo generated by kspost.60.activities.py"
+print "cat >$INSTALL_ROOT/usr/share/glib-2.0/schemas/sugar_activity_group.oob.gschema.override <<EOF"
+print "[org.sugarlabs.update]"
+print "backend='microformat.MicroformatUpdater'"
+print "microformat-update-url='%s'" % grpurl
+print "EOF"
+print "/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas"

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -135,10 +135,3 @@ if systemwide:
     print "echo '%s' > $INSTALL_ROOT/etc/olpc-update/activity-groups" % baseurl
 else:
     print "echo '%s' > $INSTALL_ROOT/home/olpc/Activities/.groups" % baseurl
-
-print "cat >/usr/share/glib-2.0/schemas/sugar_activity_group.oob.gschema.override <<EOF"
-print "[org.sugarlabs.update]"
-print "backend='microformat.MicroformatUpdater'"
-print "microformat-update-url='%s'" % baseurl
-print "EOF"
-print "/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas"