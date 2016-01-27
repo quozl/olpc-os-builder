4e02c60c4d3342319a9135280cbb6f570d169a9b
James Cameron <quozl@laptop.org>
2016-01-27 15:59:32 +1100

    debian: fix missing package detector
    Detector did not work, because scripts are run with -e; which(1) caused
    script to immediately exit without printing error.
    
    Capture the return status of which(1), build a list of missing packages,
    and trim the trailing comma before echo.

--- modules/debian/build.50.get_rootfs.sh
+++ modules/debian/build.50.get_rootfs.sh
@@ -4,13 +4,15 @@
 # the following sources os-builder-root/lib/shlib.sh (OOB__shlib is in env)
 . $OOB__shlib
 
+missing=
 for x in debootstrap make gcc zip; do
-   which $x >/dev/null
-   if [ $? -ne 0 ]; then
-      echo -e "\nPlease install $x and run the script again"
-      exit 1
-   fi
+   which $x >/dev/null || missing="$x, $missing"
 done
+if [ ! -z "$missing" ]; then
+    missing=${missing:: -2}
+    echo -e "\nMissing packages, please install $missing." >&2
+    exit 1
+fi
 
 debian_release=$(read_config debian debian_release)
 mkdir -p $cachedir/rootfs
